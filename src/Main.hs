389e527f2abcd76ad4fb6422a930a2d41099798d
nastya13 <nastya94-94.08@mail.ru>
2015-02-27 12:07:46 +0300

    function of degree is implemented

--- src/Main.hs
+++ src/Main.hs
@@ -3,7 +3,14 @@ module Main where
 
 -- function of degree
 pow :: Int -> Int -> Int
-pow a b = a
+pow a b = let 
+	    go a b r = if b < 1
+			then r
+			else 
+			  let r' = a * r
+			      b' = b - 1
+			  in go a b' r'
+	  in go a b 1
 
 -- function of factorial
 fac :: Int -> Int -> Int
@@ -13,4 +20,4 @@ fac a b = a
 bin :: Int -> Int -> Int -> Int
 bin a b c = a
 
-main = print ("Ok")
+main = print (pow 2 10)
