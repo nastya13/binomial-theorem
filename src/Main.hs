b1504b19469c94130397d5466ccd6d740fe62065
nastya13 <nastya94-94.08@mail.ru>
2015-03-05 18:25:05 +0300

    function of binomial coefficient completed with method tail-recursion

--- src/Main.hs
+++ src/Main.hs
@@ -1,15 +1,25 @@
 module Main where
 
--- function of factorial
-fac :: Int -> Int
-fac n = go n 1
-	where
-	  go 0 k = k
-	  go 1 k = k
-	  go n k = go (n-1) (k*n)
-
--- function of binomial-coefficient
+-----------------------------------------------------------------
 bin :: Int -> Int -> Int
-bin n k = div (fac n) ((fac k)*(fac (n-k)))
-
-main = print (bin 3 3)
+bin n k = 
+	if k > n 
+	  then 0
+	  else let 
+		 improve x = if x == 1 || x == 0
+				then 1
+				else x - 1
+		 notZero a = if a == 0
+				then 1
+				else a
+		 go res n' k' =
+		    if n' == 1 && k' == 1
+		      then res
+		      else let n'' = improve n'
+			       k'' = improve k'
+			       z = notZero (n'' - k'')
+			       res' = div (res * n'') (k'' * z) 
+			   in go res' n'' k''
+		in go 1 n k		
+		  
+main = print (bin 0 0)
